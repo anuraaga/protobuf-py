a27f9886536bf5664760f356584a7085c624d39f
Anuraag Agrawal <anuraaga@gmail.com>
2026-09-25 14:55:36 +0900

    fix lint

--- src/protobuf/_from_binary.py
+++ src/protobuf/_from_binary.py
@@ -295,6 +295,7 @@ def read_map_entry(
 
     key: Any = None
     value: Any = None
+    message_value: Message | None = None
 
     while reader.offset < end:
         tag = reader.tag()
@@ -324,9 +325,12 @@ def read_map_entry(
                 case DescMessage():
                     # A repeated value field within one entry merges into the
                     # previous value, like any other singular message field.
-                    if value is None:
-                        value = field_value.value.type()
-                    read_message(value, reader, opts, depth + 1, length=reader.varint())
+                    if message_value is None:
+                        message_value = field_value.value.type()
+                    read_message(
+                        message_value, reader, opts, depth + 1, length=reader.varint()
+                    )
+                    value = message_value
                 case _:
                     assert_never(field_value.value)
         else:
